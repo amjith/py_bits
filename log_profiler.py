7f934d21ee0424c4fb071d02df3e5341c645644c
Amjith <amjith@osnews.com>
2009-10-15 23:28:57 -0600

    Compile success, Debug log added, buggy.

--- log_profiler.py
+++ log_profiler.py
@@ -28,25 +28,33 @@ import re
 
 
 err = sys.stderr
-_debug = 0
+_debug = 1
 log = sys.stdout
-test_stat = {}
-test_name = "Header"
+#test_stat = {}
+#test_name = "Header"
 
 def usage():
 	print >>err, __doc__
 
 
-def reset():
-	print "Reset()"
-	test_stat = {}
-	test_name = "Header"
-	test_stat[test_name] = [0,0,0]
+def reset(test_stat, test_name):
+	#print "Reset()"
+	test_stat = {} # Reset the dictionary
+	test_name = "Header" # test_name is reset to header
+	test_stat[test_name] = [0,0,0] # Create a dictionary item for header
+	return test_stat, test_name
+
+def debug_log(string, var):
+	if _debug:
+		print >>log, string, var
+
 
 def main(argv):                          
 
 	outf = sys.stdout
 	inp_file = None
+	test_stat = {}
+	test_name = "Header"
 	try:                                
 		opts, args = getopt.getopt(argv, "ho:d ",["help","output="])
 	except getopt.GetoptError, error_string:          
@@ -61,8 +69,7 @@ def main(argv):
 			global _debug
 			_debug = 1
 		elif opt in ("-o", "--output"): 
-			if (_debug):
-				print >>log, "Output File:",arg
+			debug_log( "Output File:",arg)
 			try:
 				outf = open(arg, 'w')
 			except IOError:
@@ -76,7 +83,7 @@ def main(argv):
 		inp_file = args[0]  # Get the first argument as the input file, if more files are specified, ignore others
 		dlog = open(inp_file, 'r')
 		if (_debug):
-			print >>log, "Input File:",inp_file
+			debug_log( "Input File:",inp_file)
 
 	EC_bin = ['*', 1,2,3,4,5,6,7,8,9]
 
@@ -143,40 +150,56 @@ def main(argv):
 		if trend.search(line):
 			match = trend.search(line)
 			trend_name.append(match.groups()[0])
+			debug_log( "Trend appended:", trend_name[-1])
 		elif series.search(line):
 			match = series.search(line)
 			series_name.append(match.groups()[0])
+			debug_log( "Series appended:", series_name[-1])
 		elif start_flow.search(line):
 			#match = start_flow.search(line)
-			reset()
+			test_stat, test_name = reset(test_stat, test_name)
 			line = dlog.readline() # Read the first line in the flow
 			num_lines += 1
+			debug_log( "num_lines:", num_lines)
 			while line:
 				test_stat[test_name][0] += 1 # Increment the first element in list
 				if test_start.search(line): # +===...===+
+					debug_log("test_start",0)
 					line = dlog.readline()  # Next line
 					if testname.search(line): # Look for test_name
 						test_name = testname.search(line).groups()[0] # | TESTNAME
+						debug_log( "Test Name:", test_name)
 					dlog.readline() # Throw away +===...===+
 					test_stat[test_name] = [0,0,0] # New key added and stats initialized
-				elif first_word.search(line).groups()[0] in trend_name: # if first word is a trend
-					test_stat[test_name][1] += 1
-				elif first_word.search(line).groups()[0] in series_name: # if first word is a series
-					test_stat[test_name][2] += 1
+					debug_log( "test_stat initialized:",test_stat[test_name])
 				elif bin_line.search(line):
 					match = bin_line.search(line)
+					debug_log("Bin line:",match.groups())
 					if match.groups()[1] in EC_bin:  # If second char matches something in EC_bin then full-flow
+						debug_log("Full Flow Pass:",1)
 						done = True
 						break
 					else:
-						reset()
+						debug_log("Full Flow Fail:",0)
+						test_stat, test_name = reset(test_stat, test_name)
 						break
+				else:
+					match = first_word.search(line)
+					if match and match.groups()[0] in trend_name: # if first word is a trend
+						test_stat[test_name][1] += 1
+						debug_log("Line is a Trend. Trend Count:",test_stat[test_name][1])
+					elif match and match.groups()[0] in series_name: # if first word is a series
+						test_stat[test_name][2] += 1
+						debug_log("Line is a Series. Series Count:",test_stat[test_name][2])
+
 				line = dlog.readline() # Read next line in flow
 				num_lines += 1
 		if done:
+			debug_log("Success:",1)
 			break
 		line = dlog.readline() # Read next line in dlog
 
+	print "Total number of line:",num_lines 
 
 
 
